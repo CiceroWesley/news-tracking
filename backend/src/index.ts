34fdbf3e7e6140bdf54aab7d959d53becb219def
Cicero Wesley <wesleycariutaba@gmail.com>
2023-10-25 16:21:05 -0300

    fix find href and title

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express, {Express, Request, Response} from 'express';
 import {createServer} from 'http';
 import {Server, Socket} from 'socket.io';
 import cors from 'cors';
-import fs from 'fs'
+
 
 import * as cheerio from 'cheerio';
 
@@ -44,7 +44,8 @@ io.on('connection', async (socket : Socket) => {
     //     console.log(err)
     //   }
     // })
-    console.log($articles[0].children[0])
+    // console.log(($articles[0].children[0] as any).data)
+    
     // $articles.each((index, el) => {
     //   // let link = $(el).children('a')
     //   // let title = $(el).children('h3').text()
@@ -55,6 +56,22 @@ io.on('connection', async (socket : Socket) => {
 
 })
 
+app.get('/test', async (req, res) => {
+    const news = await axiosInstance.get(`https://news.google.com/search?q=israel&hl=pt-BR&gl=BR&ceid=BR%3Apt-419`);
+    
+    const $ = cheerio.load(news.data);
+    
+    const $articles = $('.DY5T1d.RZIKme')
+    // const articlesSlice = $articles
+    // console.log($articles[0].attribs.href)
+    // console.log((($articles[0].children[0] as unknown) as Text).data)
+    $articles.each((index, article) => {
+      console.log(index)
+      console.log(article.attribs.href)
+      console.log(((article.children[0] as unknown) as Text).data)
+    })
+    res.status(200)
+})
 
 httpServer.listen(PORT, () => {
   console.log('Server running');
